2d92b9aae356d21b61b09a0b7927c165e1f4bf7a
Jignesh Prajapati <jignesh.prajapati@plutustec.com>
2023-07-14 16:57:03 +0530

    fix mobile menu issues

--- public/js/custom.js
+++ public/js/custom.js
@@ -207,13 +207,13 @@ var showModal = function () {
     
     modal.classList.toggle("hidden");
 
-    if (!modal.classList.contains("hidden")) {
-        // Disable scroll
-        body.style.overflow = "hidden";
-    } else {
-        // Enable scroll
-        body.style.overflow = "auto";
-    }
+    // if (!modal.classList.contains("hidden")) {
+    //     // Disable scroll
+    //     body.style.overflow = "hidden";
+    // } else {
+    //     // Enable scroll
+    //     body.style.overflow = "auto";
+    // }
 };
 
 
